90df082795bc14823dcfcc2871eaf35f10e2d665
bahittinn <bahittin84@gmail.com>
2023-12-18 22:07:25 +0300

    ExploreView setup

---
+++ Airbnb-Clone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ListingItemView.swift
+//  Airbnb-Clone
+//
+//  Created by Bahittin on 18.12.2023.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingItemView()
+    }
+}
