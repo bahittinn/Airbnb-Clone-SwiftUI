9f051a7a17cfe14ed268830b96f76cd9809e8c5b
bahittinn <bahittin84@gmail.com>
2023-12-18 22:30:49 +0300

    ExploreView ui design

--- Airbnb-Clone/Core/Listings/ListingItemView.swift
+++ Airbnb-Clone/Core/Listings/ListingItemView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    
+    var images = ["image1",
+                  "image2",
+                  "image3",
+                  "image4"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            // images
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
+            // listing details
+            
+            HStack(alignment: .top) {
+                // details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                    
+                }
+                
+                Spacer()
+                
+                // rating
+                
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        } 
     }
 }
 
